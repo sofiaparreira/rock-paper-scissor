878cd61a41174f60a7fd5a7491c4f95fd45af52b
unknown <sofiapparreira@gmail.com>
2024-07-31 19:28:57 -0300

    game logi working

--- src/App.jsx
+++ src/App.jsx
@@ -5,56 +5,63 @@ import rockHandImage from './assets/rock-hand-transparent.png';
 import scissorHandImage from './assets/scissor-hand-transparent.png';
 
 function App() {
-  const [winner, setWinner] = useState('')
-  const [possibleMoves, setPossibleMoves] = useState([
+  const [winner, setWinner] = useState('');
+  const [machineMove, setMachineMove] = useState('');
+  const [resultMessage, setResultMessage] = useState('');
+  const [possibleMoves] = useState([
     {
       type: 'paper',
       label: 'Papel',
       wins: 'rock',
-      lose: 'scissor'
+      lose: 'scissor',
+      image: paperHandImage
     },
     {
       type: 'rock',
       label: 'Pedra',
       wins: 'scissor',
-      lose: 'paper'
+      lose: 'paper',
+      image: rockHandImage
     },
     {
       type: 'scissor',
       label: 'Tesoura',
       wins: 'paper',
-      lose: 'rock'
+      lose: 'rock',
+      image: scissorHandImage
     }
   ]);
 
   const makeMove = (playerMove) => {
+    const randomMoveIndex = Math.floor(Math.random() * possibleMoves.length);
+    const machineMoveType = possibleMoves[randomMoveIndex].type;
+    setMachineMove(machineMoveType);
 
-    const minimumMoveNumber = 1;
-    const maximumMoveNumber = 3;
-    const randomMove = Math.floor(Math.random() * (maximumMoveNumber - minimumMoveNumber + 1)) + minimumMoveNumber;
-
-    let moveType = '';
-    switch(randomMove) {
-      case 1: 
-        moveType = 'paper';
-        break;
-      case 2: 
-        moveType = 'rock';
-        break;
-      case 3: 
-        moveType = 'scissor';
-        break;
-      default:
-        moveType = '';
+    if (machineMoveType === playerMove) {
+      setWinner('Empate');
+      setResultMessage('Empate');
+      return;
     }
 
-    console.log(moveType);
+    const playerMoveValidation = possibleMoves.find(
+      currentMove => currentMove.type === playerMove
+    );
+
+    const isPlayerTheWinner = playerMoveValidation.wins === machineMoveType;
+
+    if (isPlayerTheWinner) {
+      setWinner('Jogador');
+      setResultMessage('Você ganhou');
+    } else {
+      setWinner('Computador');
+      setResultMessage('Você perdeu');
+    }
   };
 
-  if(moveType = playerMove) {
-    setWinner('Empate')
-    return
-  } 
+  const getMachineMoveImage = () => {
+    const move = possibleMoves.find(move => move.type === machineMove);
+    return move ? move.image : null;
+  };
 
   return (
     <div className='w-screen h-screen bg-slate-800 mx-0 text-white pt-16'>
@@ -66,19 +73,32 @@ function App() {
         </div>
       </div>
 
-      <div>
-        <button onClick={() => makeMove('paper')}>
+      <div className='flex justify-center mt-8'>
+        <button className='mx-4' onClick={() => makeMove('paper')}>
           <img src={paperHandImage} alt="Paper" />
         </button>
 
-        <button onClick={() => makeMove('rock')}>
+        <button className='mx-4' onClick={() => makeMove('rock')}>
           <img src={rockHandImage} alt="Rock" />
         </button>
 
-        <button onClick={() => makeMove('scissor')}>
+        <button className='mx-4' onClick={() => makeMove('scissor')}>
           <img src={scissorHandImage} alt="Scissor" />
         </button>
       </div>
+
+      {machineMove && (
+        <div className='text-center mt-8'>
+          <h2 className='text-2xl'>A máquina escolheu:</h2>
+          <img src={getMachineMoveImage()} alt={machineMove} className='mx-auto' />
+        </div>
+      )}
+
+      {resultMessage && (
+        <div className='text-center mt-8'>
+          <h2 className='text-2xl'>{resultMessage}</h2>
+        </div>
+      )}
     </div>
   );
 }
